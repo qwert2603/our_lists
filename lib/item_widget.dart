a9081893f82ae542bd88db73a21f5abc5447383d
qwert2603 <qwert2603@mail.ru>
2020-11-01 17:02:08 +0700

    don't consume long press on checkBox and fav-icon

--- lib/item_widget.dart
+++ lib/item_widget.dart
@@ -11,44 +11,43 @@ class ItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //todo: ripple on touch
-    //todo: don't consume long press on checkBox and fav-icon
     return Padding(
       padding: EdgeInsets.all(2),
-      child: GestureDetector(
-        child: Row(
-          children: [
-            Checkbox(
-              value: item.isChecked,
-              onChanged: (ch) =>
-                  context.repo.setItemChecked(item.listId, item.id, ch),
-            ),
-            const SizedBox(width: 4),
-            Expanded(
+      child: Row(
+        children: [
+          Checkbox(
+            value: item.isChecked,
+            onChanged: (ch) =>
+                context.repo.setItemChecked(item.listId, item.id, ch),
+          ),
+          const SizedBox(width: 4),
+          Expanded(
+            child: GestureDetector(
+              onLongPress: () async {
+                final delete = await showDialog(
+                  context: context,
+                  child: DeleteItemDialog(
+                    itemName: item.name,
+                  ),
+                );
+                if (delete == true) {
+                  context.repo.removeItem(item.listId, item.id);
+                }
+              },
               child: Text(
                 item.name,
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            IconButton(
-              icon: Icon(
-                item.isFavourite ? Icons.star : Icons.star_border,
-              ),
-              onPressed: () => context.repo
-                  .setItemFavourite(item.listId, item.id, !item.isFavourite),
+          ),
+          IconButton(
+            icon: Icon(
+              item.isFavourite ? Icons.star : Icons.star_border,
             ),
-          ],
-        ),
-        onLongPress: () async {
-          final delete = await showDialog(
-            context: context,
-            child: DeleteItemDialog(
-              itemName: item.name,
-            ),
-          );
-          if (delete == true) {
-            context.repo.removeItem(item.listId, item.id);
-          }
-        },
+            onPressed: () => context.repo
+                .setItemFavourite(item.listId, item.id, !item.isFavourite),
+          ),
+        ],
       ),
     );
   }
