1fb957c285e276fd8c8448fd2dccb0fbe0605ae9
Alexander Zhdanov <qwert2603@mail.ru>
2021-05-20 19:28:59 +0500

    fix mapping of nullable fields from firestore

--- lib/mappers.dart
+++ lib/mappers.dart
@@ -2,13 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:our_lists/model.dart';
 
 extension DocumentX on DocumentSnapshot {
-  Item toItem({required String listId}) => Item(
-        id: id,
-        name: get("name") as String,
-        listId: listId,
-        isChecked: get("is_checked") as bool? ?? false,
-        isFavourite: get("is_favourite") as bool? ?? false,
-      );
+  Item toItem({required String listId}) {
+    final Map<String, dynamic> map = data() as Map<String, dynamic>;
+    return Item(
+      id: id,
+      name: map["name"] as String,
+      listId: listId,
+      isChecked: map["is_checked"] as bool? ?? false,
+      isFavourite: map["is_favourite"] as bool? ?? false,
+    );
+  }
 
   ItemsList toItemsList() => ItemsList(
         id: id,
